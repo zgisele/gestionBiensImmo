f1bcb94f4f06d2de35ebecbaf426151558cd8711
zgisele <kafouigisele@gmail.com>
2023-11-21 08:50:34 +0000

    recupération des branchs

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,30 @@ use App\Http\Controllers\CommentaireController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::post('/article/articles', [ArticleController::class, 'store']);
+Route::get('/article/listeArticle', [ArticleController::class, 'show']);
+Route::get('/article/{id}', [ArticleController::class, 'voirDetails']);
+Route::patch('/articleModif/{id}', [ArticleController::class, 'update']);//permet de renvoyer le formulaire avec patch
+Route::get('/modifier/{id}', [ArticleController::class, 'edit']);//permet de renvoyer la vue qui permet de modifier article
+Route::delete('/articleSupprimer/{id}', [ArticleController::class, 'destroy']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/commentaire', [CommentaireController::class, 'index']);
+
+Route::post('/commentaires/{id}', [CommentaireController::class, 'store']);
+Route::get('/commentaire', [CommentaireController::class, 'show']);
+
+Route::get('/commentaire/{id}/modifier', [CommentaireController::class, 'edit'])->name('commentaire.modifier');
+Route::put('/commentaire/{id}', [CommentaireController::class, 'update'])->name('commentaire.update');
+
+
+
+Route::delete('/commentaires/{id}', [CommentaireController::class, 'destroy']);
+
+
+
+
